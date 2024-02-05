d17d6da306544943f0604ab3dfadfb24f861cf48
mas0110 <masoudmohammed13@gmail.com>
2024-02-05 21:15:44 +0100

    logica lista

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,36 @@
 const {
-    createApp 
-} = Vue 
+    createApp
+} = Vue
 
 createApp({
     data() {
         return {
+            mail: [],
+            listMail: [],
         }
     },
-    created(){
+    created() {
+        console.log(this.mail)
+        console.log(this.listMail)
+        this.generaMail()
     },
-    methods:{
+    methods: {
+        async generaMail() {
+
+            for (let i = 0; i < 10; i++) {
+                const result = await axios.get('https://flynn.boolean.careers/exercises/api/random/mail');
+                this.mail.push(result.data.response);
+            }
+        },
+        stampaMail() {
+
+            for (let i = 0; i < 10; i++) {
+                this.listMail.push(this.mail[i]);
+            }
+        },
+        cancellaLista() {
+            window.location.reload();
+
+        }
     },
 }).mount('#app')
